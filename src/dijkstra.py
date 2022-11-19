7ef244569f349c82e7ed545be25f5368ceef906f
Maija <maija.saisa@helsinki.fi>
2022-11-19 21:42:52 +0200

    alku- ja loppupisteen valinta hiirellä

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,16 +1,15 @@
 class Dijkstra:
-    def __init__(self, ruudukko, alku, maali):
+    def __init__(self, ruudukko):
         self.ruudukko = ruudukko
-        self.alku = alku
-        self.maali = maali
-        self.jono = [alku]
+        self.alku = None
+        self.jono = []
         self.reitti = []
     
     def lyhin_dijkstra(self):
         if len(self.jono) > 0:
             ruutu = self.jono.pop(0)
             ruutu.vierailtu = True
-            if ruutu == self.maali:
+            if ruutu.maali:
                 while ruutu.edellinen != self.alku:
                     self.reitti.append(ruutu.edellinen)
                     ruutu = ruutu.edellinen
--- src/kayttoliittyma.py
+++ src/kayttoliittyma.py
@@ -3,20 +3,24 @@ from PIL import Image
 from ruutu import Ruutu
 from dijkstra import Dijkstra
 
+# RGB-värejä
 MUSTA = (0,0,0)
 VALKOINEN = (255,255,255)
 PUNAINEN = (255,0,0)
 VIHREA = (0,255,0)
 SININEN = (0,0,255)
+TURKOOSI = (0,200,200)
+KELTAINEN = (200,200,0)
 
 def kayttoliittyma():
+    # Kartan & reitin piirtämisen alustus
     kuva = Image.open("kartta.png")
+    kuva.save("reitti.png")
     pikselikartta = kuva.load()
     leveys, korkeus = kuva.size
-    kuva.save("reitti.png")
-    ruudukko = []
-        
+    
     # Luo ruudukko
+    ruudukko = []
     for y in range(korkeus):
         rivi = []
         for x in range(leveys):
@@ -33,49 +37,79 @@ def kayttoliittyma():
             ruutu = ruudukko[y][x]
             if not ruutu.seina:
                 ruutu.lisaa_naapurit(ruudukko)
-        
-    # Aseta lähtöruutu manuaalisesti
-    lahtoruutu = ruudukko[10][10]
-    lahtoruutu.alku = True
-    lahtoruutu.vierailtu = True
-
-    # Aseta maaliruutu manuaalisesti
-    maaliruutu = ruudukko[93][93]
-    maaliruutu.maali = True
     
     pygame.init()
     pygame.display.set_caption("Reitinhaku")
-    naytto = pygame.display.set_mode((2100, 1100))
-    haku = Dijkstra(ruudukko, lahtoruutu, maaliruutu)
-    etsi = True
+    ikkunan_leveys = 2100
+    ikkunan_korkeus = 1100
+    ikkuna = pygame.display.set_mode((ikkunan_leveys, ikkunan_korkeus))
+    positio_kartta = (100,100) #(x,y)
+    skaalauskerroin = 9
+    skaalattu_kartta = (leveys*skaalauskerroin, korkeus*skaalauskerroin)
+    
+    haku = Dijkstra(ruudukko)
+    hae = True
+    etsi = False
+    piirra = True
+    valittu_alku = False
+    valittu_loppu = False
 
     while True:
         for tapahtuma in pygame.event.get():
             if tapahtuma.type == pygame.QUIT:
                 exit()
-
+            elif tapahtuma.type == pygame.MOUSEBUTTONDOWN:
+                x, y = tapahtuma.pos
+                x_kartta = round((x-positio_kartta[0]) / skaalauskerroin)
+                y_kartta = round((y-positio_kartta[1]) / skaalauskerroin)
+                # Aseta alkupiste
+                if pygame.mouse.get_pressed()[0] and not valittu_alku:
+                    if x_kartta in range(leveys) and y_kartta in range(korkeus):
+                        ruutu = ruudukko[y_kartta][x_kartta]
+                        if not ruutu.seina:
+                            ruutu.alku = True
+                            ruutu.vierailtu = True
+                            haku.alku = ruutu
+                            haku.jono.append(ruutu)
+                            valittu_alku = True
+                # Aseta loppupiste
+                elif pygame.mouse.get_pressed()[0] and valittu_alku and not valittu_loppu:
+                    if x_kartta in range(leveys) and y_kartta in range(korkeus):
+                        ruutu = ruudukko[y_kartta][x_kartta]
+                        if not ruutu.seina:
+                            ruutu.maali = True
+                            valittu_loppu = True
+                # Käynnistä haku & animaatio
+                elif pygame.mouse.get_pressed()[0] and valittu_alku and valittu_loppu:
+                    etsi = True           
+                            
         if etsi:
-            etsi = haku.lyhin_dijkstra()
+            hae = haku.lyhin_dijkstra()
 
-        naytto.fill(MUSTA)
+        ikkuna.fill(MUSTA)
         
-        for y in range(leveys):
-            for x in range(korkeus):
-                ruutu = ruudukko[y][x]
-                if ruutu.jonossa:
-                    pikselikartta[x,y] = PUNAINEN
-                if ruutu.vierailtu:
-                    pikselikartta[x,y] = VIHREA
-                if ruutu in haku.reitti:
-                    pikselikartta[x,y] = SININEN
-                if ruutu.alku:
-                    pikselikartta[x,y] = (0,200,200)
-                if ruutu.maali:
-                    pikselikartta[x,y] = (200,200,0)
+        if piirra:
+            for y in range(korkeus):
+                for x in range(leveys):
+                    ruutu = ruudukko[y][x]
+                    if ruutu.jonossa:
+                        pikselikartta[x,y] = KELTAINEN
+                    if ruutu.vierailtu:
+                        pikselikartta[x,y] = TURKOOSI
+                    if ruutu in haku.reitti:
+                        pikselikartta[x,y] = SININEN
+                    if ruutu.alku:
+                        pikselikartta[x,y] = PUNAINEN
+                    if ruutu.maali:
+                        pikselikartta[x,y] = VIHREA
+            kuva.save("reitti.png")
+        
+        if hae == False:
+            piirra = False
+            etsi = False
             
-        kuva.save("reitti.png")
-        kartta = pygame.transform.scale(pygame.image.load("reitti.png"), (900,900))
-        naytto.blit(kartta, (100,100))
+        kartta = pygame.transform.scale(pygame.image.load("reitti.png"), skaalattu_kartta)
+        ikkuna.blit(kartta, positio_kartta)
         
         pygame.display.flip()        
                 
